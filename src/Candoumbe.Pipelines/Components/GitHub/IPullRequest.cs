334e0bfaa29ac42a682176854e3ef2409808c0ab
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2022-11-04 16:43:17 +0100

    Changed current branch name

--- src/Candoumbe.Pipelines/Components/GitHub/IPullRequest.cs
+++ src/Candoumbe.Pipelines/Components/GitHub/IPullRequest.cs
@@ -17,18 +17,25 @@ namespace Candoumbe.Pipelines.Components.GitHub
     /// <summary>
     /// This interface adds a target to open a pull request
     /// </summary>
-    public interface IPullRequest : IGitFlow
+    public interface IPullRequest : IGitFlow, IHaveGitHubRepository
     {
         /// <summary>
         /// The title of the PR that will be created
         /// </summary>
         [Parameter("Title that will be used when creating a PR")]
-        string Title => TryGetValue(() => Title) ?? (GitRepository.IsOnFeatureBranch(), GitRepository.IsOnReleaseBranch(), GitRepository.IsOnHotfixBranch()) switch
+        string Title => TryGetValue(() => Title) ?? ((GitRepository.IsOnFeatureBranch(), GitRepository.IsOnReleaseBranch(), GitRepository.IsOnHotfixBranch()) switch
         {
             (true, _, _) => $"[FEATURE] {GitRepository.Branch.ToTitleCase()}",
             (_, _, true) => $"[HOTFIX] {GitRepository.Branch.ToTitleCase()}",
             _ => GitRepository.Branch.ToTitleCase()
-        };
+        }).Replace('-', ' ');
+
+        /// <summary>
+        /// Token that will be used to connect to GitHub
+        /// </summary>
+        [Parameter("Token used to create a pull request")]
+        [Secret]
+        string Token => TryGetValue(() => Token);
 
         /// <summary>
         /// Description of the pull request
@@ -53,20 +60,19 @@ namespace Candoumbe.Pipelines.Components.GitHub
             GitPushToRemote();
 
             string repositoryName = GitRepository.GetGitHubName();
-            string branchName = GitRepository.Branch;
+            string branchName = GitCurrentBranch();
             string owner = GitRepository.GetGitHubOwner();
 
             Information("Creating a pull request for {Repository}", repositoryName);
-
             string title = PromptForInput("Title of the pull request :", Title);
 
             Information("Creating {PullRequestName} for {Repository}", title, repositoryName);
-            string username = PromptForInput("Username", owner);
-            string password = PromptForInput("Password", string.Empty);
+            string token = Token ?? PromptForInput("Token (leave empty to exit)", string.Empty);
 
+            Information("{SourceBranch} ==> {TargetBranch} on the behalf of {UserName}", branchName, DevelopBranch, token);
             GitHubClient gitHubClient = new(new ProductHeaderValue(repositoryName))
             {
-                Credentials = new Credentials(username, password, AuthenticationType.Basic)
+                Credentials = new Credentials(token)
             };
 
             NewPullRequest newPullRequest = new(title, branchName, DevelopBranch)
