a8ea2ee81e8c5b7aa02c64affe64c9f7cc667ca5
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-08-22 22:43:29 +0200

    refactor(mutation)! :  refactoring of `IMutationTest.MutationTests`
    - `IMutationTest` component no longer implements `IUnitTest` but only `IHaveTests`
    
    - Refactoring of `IMutationTest` component to improve maintenability : the way CLI options required by Stryker are computed is now centralized.

--- src/Candoumbe.Pipelines/Components/IMutationTest.cs
+++ src/Candoumbe.Pipelines/Components/IMutationTest.cs
@@ -4,6 +4,7 @@ using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Utilities;
 
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,6 @@ using System.Linq;
 
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Serilog.Log;
-
 namespace Candoumbe.Pipelines.Components;
 
 /// <summary>
@@ -22,7 +22,7 @@ namespace Candoumbe.Pipelines.Components;
 /// Simply adds <c>&lt;PackageReference Include="dotnet-stryker" Version="" ExcludeAssets="all" &gt;</c> to your build project file
 /// </remarks>
 [NuGetPackageRequirement("dotnet-stryker")]
-public interface IMutationTest : IUnitTest
+public interface IMutationTest : IHaveTests
 {
     /// <summary>
     /// Name of the property set when <see href="SourceLink">SourceLink</see> is enabled on a projet.
@@ -46,7 +46,7 @@ public interface IMutationTest : IUnitTest
     /// <remarks>
     /// The source project has
     /// </remarks>
-    IEnumerable<(Project SourceProject, IEnumerable<Project> TestProjects)> MutationTestsProjects { get; }
+    IEnumerable<MutationProjectConfiguration> MutationTestsProjects { get; }
 
     /// <summary>
     /// Executes mutation tests for the specified <see cref="MutationTestsProjects"/>.
@@ -56,146 +56,141 @@ public interface IMutationTest : IUnitTest
         .Description("Runs mutation tests using Stryker tool")
         .TryDependsOn<IClean>(x => x.Clean)
         .TryBefore<IPack>()
-        .DependsOn(Compile)
+        .TryDependsOn<ICompile>(x => x.Compile)
         .Produces(MutationTestResultDirectory / "*")
         .Executes(() =>
         {
-            int mutationProjectCount = 0;
-
-            // List of frameworks that source project are tested against
-            string[] frameworks = MutationTestsProjects.Select(csprojAndTest => csprojAndTest.TestProjects)
-                                                       .SelectMany(projects => projects)
-                                                       .Select(csproj => csproj.GetTargetFrameworks())
-                                                       .SelectMany(x => x)
-                                                       .Select(targetFramework => targetFramework.ToLower().Trim())
-                                                       .AsParallel()
-                                                       .Distinct()
-                                                       .ToArray();
-
+            // List of frameworks that source projects are tested against
+            IReadOnlyCollection<string> frameworks = MutationTestsProjects.Select(csprojAndTest => csprojAndTest.TestProjects)
+                                                                          .SelectMany(projects => projects)
+                                                                          .Select(csproj => csproj.GetTargetFrameworks())
+                                                                          .SelectMany(x => x)
+                                                                          .Select(targetFramework => targetFramework.Trim())
+                                                                          .AsParallel()
+                                                                          .Distinct(StringComparer.OrdinalIgnoreCase)
+                                                                          .ToArray();
 
             if (frameworks.AtLeast(2))
             {
-                MutationTestsProjects.ForEach(tuple =>
+                // We iterate over each test project
+                MutationTestsProjects.ForEach(mutationProject =>
                 {
-                    mutationProjectCount++;
-                    (Project sourceProject, IEnumerable<Project> testsProjects) = tuple;
-                    AbsolutePath mutationTestOutputDirectory = MutationTestResultDirectory / sourceProject.Name;
+                    // We retrieve the current set of frameworks the current test project are tested against
+                    IReadOnlyCollection<string> testedFrameworks = mutationProject.TestProjects.Select(csproj => csproj.GetTargetFrameworks())
+                                                                                               .SelectMany(frameworks => frameworks)
+                                                                                               .Distinct(StringComparer.OrdinalIgnoreCase)
+                                                                                               .ToArray();
 
-                    IReadOnlyCollection<string> testedFrameworks = testsProjects.Select(csproj => csproj.GetTargetFrameworks())
-                                                                                   .SelectMany(x => x)
-                                                                                   .Distinct()
-                                                                                   .ToArray();
-                    Arguments args;
-                    if (testedFrameworks.AtLeast(2))
-                    {
-                        testedFrameworks.ForEach(framework =>
-                        {
-                            args = new();
-                            args.Apply(StrykerArgumentsSettingsBase)
-                                .Apply(StrykerArgumentsSettings);
-
-                            args.Add("--target-framework {value}", framework)
-                                .Add("--output {value}", mutationTestOutputDirectory / framework);
-
-                            RunMutationTestsForTheProject(sourceProject, testsProjects, args);
-                        });
-                    }
-                    else
-                    {
-                        args = new();
-                        string framework = testedFrameworks.Single();
-                        args.Add("--target-framework {value}", framework)
-                            .Add("--output {value}", mutationTestOutputDirectory / framework);
-
-                        RunMutationTestsForTheProject(sourceProject, testsProjects, args);
-                    }
+                    testedFrameworks.ForEach(framework => RunMutationTestsForTheProject(mutationProject, framework));
                 });
             }
             else
             {
+                string framework = frameworks.Single();
+
+                MutationTestsProjects.ForEach(mutationProject => RunMutationTestsForTheProject(mutationProject, framework));
+            }
+
+            /// <summary>
+            /// Run mutation tests for the specified project using the specified arguments
+            /// </summary>
+            void RunMutationTestsForTheProject(MutationProjectConfiguration mutationProject, string framework)
+            {
+                (Project sourceProject, IEnumerable<Project> testsProjects, AbsolutePath configFile) = mutationProject;
+
+                Verbose("{ProjetName} will run mutation tests for the following frameworks : {@Frameworks}", sourceProject.Name, sourceProject.GetTargetFrameworks());
+
+                MutationTestRunConfiguration mutationTestRunConfiguration = (configFile?.FileExists() is true
+                                ? configFile.ReadJson<MutationTestRunConfiguration>()
+                                : null);
+
                 Arguments args = new();
-                args.Apply(StrykerArgumentsSettingsBase)
-                    .Apply(StrykerArgumentsSettings);
+                args = args.Apply(StrykerArgumentsSettingsBase)
+                           .Apply(StrykerArgumentsSettings);
 
-                args.Add("--target-framework {value}", frameworks.Single());
+                args.Add("--target-framework {value}", framework)
+                    .Add("--output {value}", MutationTestResultDirectory / sourceProject.Name / framework);
 
-                MutationTestsProjects.ForEach(tuple =>
+                Arguments strykerArgs = new();
+                strykerArgs = strykerArgs.Add("stryker");
+
+                strykerArgs = strykerArgs.Concatenate(args);
+
+                strykerArgs.Add(@"--project {value}", sourceProject.Name);
+
+
+                testsProjects.ForEach(project =>
                 {
-                    mutationProjectCount++;
-                    args = args.Add("--output {value}", MutationTestResultDirectory / tuple.SourceProject.Name);
-                    RunMutationTestsForTheProject(tuple.SourceProject, tuple.TestProjects, args);
+                    strykerArgs = strykerArgs.Add(@"--test-project {value}", project.Path);
                 });
+
+                switch (this)
+                {
+                    case IGitFlow gitFlow when gitFlow.GitRepository is { } gitflowRepository:
+                        {
+                            strykerArgs = strykerArgs.Add("--version {value}", gitflowRepository.Commit ?? gitflowRepository.Branch);
+                            switch (gitflowRepository.Branch)
+                            {
+                                case string branchName when string.Equals(branchName, IGitFlow.DevelopBranchName, StringComparison.InvariantCultureIgnoreCase):
+                                    {
+                                        // we are in git flow so we can compare develop with main branch
+                                        strykerArgs = strykerArgs.Add("--with-baseline:{value}", IGitFlow.MainBranchName);
+                                    }
+                                    break;
+                                case string branchName when branchName.Like($"{gitFlow.FeatureBranchPrefix}/*", true):
+                                    {
+                                        strykerArgs = strykerArgs.Add("--with-baseline:{value}", gitFlow.FeatureBranchSourceName);
+                                    }
+                                    break;
+                                case string branchName when branchName.Like($"{gitFlow.ColdfixBranchPrefix}/*", true):
+                                    {
+                                        strykerArgs = strykerArgs.Add("--with-baseline:{value}", gitFlow.ColdfixBranchSourceName);
+                                    }
+                                    break;
+                                default:
+                                    {
+                                        // By default we try to compare the current commit with the previous one (if any).
+                                        if (gitflowRepository.Head is string head)
+                                        {
+                                            strykerArgs = strykerArgs.Add("--with-baseline:{value}", head);
+                                        }
+                                    }
+                                    break;
+                            }
+
+                            break;
+                        }
+
+                    case IGitHubFlow gitHubFlow when gitHubFlow.GitRepository is { } githubFlowRepository:
+                        {
+                            strykerArgs = strykerArgs.Add("--version {value}", githubFlowRepository.Commit ?? githubFlowRepository.Branch);
+                            if (githubFlowRepository.Branch is { Length: > 0 } branchName && !string.Equals(branchName, IGitHubFlow.MainBranchName, StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                strykerArgs = strykerArgs.Add("--with-baseline:{0}", IGitHubFlow.MainBranchName);
+                            }
+
+                            break;
+                        }
+
+                    default:
+                        if (!sourceProject.IsSourceLinkEnabled())
+                        {
+                            if (this is IHaveGitVersion gitVersion)
+                            {
+                                strykerArgs = strykerArgs.Add("--version {value}", gitVersion.MajorMinorPatchVersion);
+                            }
+                            else if (this is IHaveGitRepository gitRepository)
+                            {
+                                strykerArgs = strykerArgs.Add("--version {value}", gitRepository.GitRepository?.Commit ?? gitRepository?.GitRepository?.Branch);
+                            }
+                        }
+
+                        break;
+                }
+
+                DotNet(strykerArgs.RenderForExecution(), workingDirectory: sourceProject.Path.Parent);
             }
         });
-
-    /// <summary>
-    /// Run mutation tests for the specified project using the specified arguments
-    /// </summary>
-    private void RunMutationTestsForTheProject(Project sourceProject, IEnumerable<Project> testsProjects, Arguments args)
-    {
-        Verbose("{ProjetName} will run mutation tests for the following frameworks : {@Frameworks}", sourceProject.Name, sourceProject.GetTargetFrameworks());
-
-        Arguments strykerArgs = new();
-        strykerArgs = strykerArgs.Add("stryker");
-        strykerArgs = strykerArgs.Concatenate(args);
-
-        strykerArgs.Add(@"--project {value}", sourceProject.Name);
-
-        testsProjects.ForEach(project =>
-        {
-            strykerArgs = strykerArgs.Add(@"--test-project {value}", project.Path);
-        });
-
-        if (this is IGitFlow gitFlow && gitFlow.GitRepository is { } gitflowRepository)
-        {
-            strykerArgs = strykerArgs.Add("--version {value}", gitflowRepository.Commit ?? gitflowRepository.Branch);
-            switch (gitflowRepository.Branch)
-            {
-                case string branchName when string.Equals(branchName, IGitFlow.DevelopBranchName, StringComparison.InvariantCultureIgnoreCase):
-                    {
-                        // we are in git flow so comparison we can compare develop against main branch
-                        strykerArgs = strykerArgs.Add("--with-baseline:{value}", IGitFlow.MainBranchName);
-
-                    }
-                    break;
-                case string branchName when branchName.Like($"{gitFlow.FeatureBranchPrefix}/*", true):
-                    {
-                        strykerArgs = strykerArgs.Add("--with-baseline:{value}", gitFlow.FeatureBranchSourceName);
-                    }
-                    break;
-                case string branchName when branchName.Like($"{gitFlow.ColdfixBranchPrefix}/*", true):
-                    {
-                        strykerArgs = strykerArgs.Add("--with-baseline:{value}", gitFlow.ColdfixBranchSourceName);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-        else if (this is IGitHubFlow gitHubFlow && gitHubFlow.GitRepository is { } githubFlowRepository)
-        {
-            strykerArgs = strykerArgs.Add("--version {value}", githubFlowRepository.Commit ?? githubFlowRepository.Branch);
-            if (githubFlowRepository.Branch is { Length: > 0 } branchName && !string.Equals(branchName, IGitHubFlow.MainBranchName, StringComparison.InvariantCultureIgnoreCase))
-            {
-                strykerArgs = strykerArgs.Add("--with-baseline:{0}", IGitHubFlow.MainBranchName);
-            }
-        }
-        else if (!sourceProject.IsSourceLinkEnabled())
-        {
-            if (this is IHaveGitVersion gitVersion)
-            {
-                strykerArgs = strykerArgs.Add("--version {value}", gitVersion.MajorMinorPatchVersion);
-            }
-            else if (this is IHaveGitRepository gitRepository)
-            {
-                strykerArgs = strykerArgs.Add("--version {value}", gitRepository.GitRepository?.Commit ?? gitRepository?.GitRepository?.Branch);
-            }
-        }
-
-        DotNet(strykerArgs.RenderForExecution(), workingDirectory: sourceProject.Path.Parent);
-
-
-    }
 
     internal Configure<Arguments> StrykerArgumentsSettingsBase => _
         => _
@@ -211,4 +206,69 @@ public interface IMutationTest : IUnitTest
     /// Configures arguments that will be used by when running Stryker tool
     /// </summary>
     Configure<Arguments> StrykerArgumentsSettings => _ => _;
+}
+
+/// <summary>
+/// Wraps information on mutation tests for a specific project.
+/// </summary>
+public record MutationProjectConfiguration
+{
+    /// <summary>
+    /// The project for which mutation tests will be run.
+    /// </summary>
+    public Project SourceProject { get; }
+
+    /// <summary>
+    /// Set
+    /// </summary>
+    public IReadOnlySet<Project> TestProjects { get; }
+
+    /// <summary>
+    /// Path to the JSON configuration file to use when running mutation tests for <see cref="SourceProject"/>
+    /// </summary>
+    public AbsolutePath ConfigurationFile { get; init; }
+
+    /// <summary>
+    /// Builds a new <see cref="MutationProjectConfiguration"/>
+    /// </summary>
+    /// <param name="sourceProject">The project onto which mutation will be performed</param>
+    /// <param name="testProjects">List of projects that will be used to validate mutation performed. These are usually</param>
+    /// <param name="configurationFile">Path to the configuration file that can be used by Stryker when running mutation tests.</param>
+    public MutationProjectConfiguration(Project sourceProject, IEnumerable<Project> testProjects, AbsolutePath configurationFile = default)
+    {
+        SourceProject = sourceProject ?? throw new ArgumentNullException(nameof(sourceProject));
+        TestProjects = testProjects.ToHashSet();
+        ConfigurationFile = configurationFile;
+    }
+
+    ///<inheritdoc/>
+    public void Deconstruct(out Project sourceProject, out IReadOnlySet<Project> testProjects, out AbsolutePath configurationFile)
+    {
+        sourceProject = SourceProject;
+        testProjects = TestProjects;
+        configurationFile = ConfigurationFile;
+    }
+}
+
+file record MutationTestRunConfiguration
+{
+    /// <summary>
+    /// Name of the module under which regroup result of the current mutation test run
+    /// </summary>
+    public string Module { get; init; }
+
+    public string[] TestsProjects { get; init; }
+
+    public string[] Frameworks { get; init; }
+
+    public StrykerProjectInfo ProjectInfo { get; init; }
+
+
+}
+
+file record StrykerProjectInfo
+{
+    public string Module { get; init; }
+
+    public string Version { get; init; }
 }
