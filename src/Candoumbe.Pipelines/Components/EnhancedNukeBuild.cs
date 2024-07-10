f5fe37e05b7a204e6d6b81a439b82fa29283edbc
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2024-07-10 16:45:10 +0200

    feat: make `EnhancedNukeBuild` abstract

--- src/Candoumbe.Pipelines/Components/EnhancedNukeBuild.cs
+++ src/Candoumbe.Pipelines/Components/EnhancedNukeBuild.cs
@@ -5,7 +5,7 @@ namespace Candoumbe.Pipelines.Components;
 /// <summary>
 /// Represents an enhanced version of the <see cref="NukeBuild"/> class which adds various options support from components.
 /// </summary>
-public class EnhancedNukeBuild : NukeBuild, ICanSkipConfirmation
+public abstract class EnhancedNukeBuild : NukeBuild, ICanSkipConfirmation
 {
     /// <inheritdoc />
     [Parameter("Set to true to not ask any confirmation to the end user (default: false)")]
