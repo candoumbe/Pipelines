6d2b8621372f220c37709d335facf736a78e9353
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2024-12-30 00:32:14 +0100

    feat(: split `IReportCoverage` component
    split `IReportCoverage` component into more specific component to handle both unit and integration tests.

--- src/Candoumbe.Pipelines/Components/IReportCoverage.cs
+++ src/Candoumbe.Pipelines/Components/IReportCoverage.cs
@@ -17,7 +17,7 @@ using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
 /// <remarks>
 /// This component requires <see href="https://nuget.org/packages/reportgenerator">ReportGenerator tool</see>
 /// </remarks>
-public interface IReportCoverage : IUnitTest, IRequireNuGetPackage
+public interface IReportCoverage : INukeBuild, IRequireNuGetPackage
 {
     /// <summary>
     /// The API key used to push code coverage to CodeCov
@@ -27,68 +27,20 @@ public interface IReportCoverage : IUnitTest, IRequireNuGetPackage
     public string CodecovToken => TryGetValue(() => CodecovToken);
 
     /// <summary>
-    /// Defines if <see cref="ReportCoverage"/> should publish results to <see href="codecov.io">Code Cov</see>.
+    /// Defines if the component should publish results to <see href="codecov.io">Code Cov</see>.
     /// </summary>
     bool ReportToCodeCov { get; }
 
     /// <summary>
-    /// Pushes code coverage to CodeCov
-    /// </summary>
-    public Target ReportCoverage => _ => _
-       .DependsOn(UnitTests)
-       .Requires(() => !ReportToCodeCov || CodecovToken != null)
-       .Consumes(UnitTests, UnitTestResultsDirectory / "*.xml")
-       .Produces(CoverageReportDirectory / "*.xml")
-       .Produces(CoverageReportHistoryDirectory / "*.xml")
-       .Executes(() =>
-       {
-           ReportGenerator(s => s.Apply(ReportGeneratorSettingsBase)
-                                 .Apply(ReportGeneratorSettings));
-
-           if (ReportToCodeCov)
-           {
-               Codecov(s => s.Apply(CodeCovSettingsBase)
-                             .Apply(CodecovSettings));
-           }
-       });
-
-    internal sealed Configure<CodecovSettings> CodeCovSettingsBase => _ => _
-        .SetFiles(UnitTestResultsDirectory.GlobFiles("*.xml").Select(x => x.ToString()))
-        .SetToken(CodecovToken)
-        .SetFramework("netcoreapp3.0")
-        .WhenNotNull(this.As<IHaveGitVersion>(),
-                     (_, version) => _.SetBuild(version.GitVersion.FullSemVer))
-        .WhenNotNull(this.As<IHaveGitRepository>(),
-                     (_, repository) => _.SetBranch(repository.GitRepository.Branch)
-                                         .SetSha(repository.GitRepository.Commit));
-
-    /// <summary>
-    /// Defines settings used by <see cref="ReportCoverage"/> target
+    /// Defines settings used by the component
     /// </summary>
     /// <remarks>
     /// These settings are only used when <see cref="ReportToCodeCov"/> is <see langword="true"/>.
     /// </remarks>
     Configure<CodecovSettings> CodecovSettings => _ => _;
 
-    internal sealed Configure<ReportGeneratorSettings> ReportGeneratorSettingsBase => _ => _
-        .SetFramework("net5.0")
-        .SetReports(UnitTestResultsDirectory / "*.xml")
-        .SetReportTypes(ReportTypes.Badges, ReportTypes.HtmlChart, ReportTypes.HtmlInline)
-        .WhenNotNull(this.As<IHaveGitRepository>()?.GitRepository,
-                     (_, repository) => !string.IsNullOrWhiteSpace(repository.Branch)
-                                        ? _.SetTargetDirectory(CoverageReportDirectory / repository.Branch)
-                                          .SetHistoryDirectory(CoverageReportHistoryDirectory / repository.Branch)
-                                          .SetTag(repository.Commit)
-                        : _.SetTargetDirectory(CoverageReportDirectory)
-                           .SetHistoryDirectory(CoverageReportHistoryDirectory)
-                           .SetTag(repository.Commit)
-                     )
-        .When(_ => this.As<IHaveGitRepository>() is null,
-              _ => _.SetTargetDirectory(CoverageReportDirectory)
-                    .SetHistoryDirectory(CoverageReportHistoryDirectory));
-
     /// <summary>
-    /// Defines settings used by <see cref="ReportCoverage"/> target
+    /// Defines settings used to report coverage
     /// </summary>
     Configure<ReportGeneratorSettings> ReportGeneratorSettings => _ => _;
 }
---
+++ src/Candoumbe.Pipelines/Components/IReportUnitTestCoverage.cs
@@ -0,0 +1,69 @@
+﻿namespace Candoumbe.Pipelines.Components;
+
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Codecov;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.ReportGenerator;
+using static Nuke.Common.Tools.Codecov.CodecovTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
+
+/// <summary>
+/// Build component that can report unit tests code coverage
+/// </summary>
+/// <remarks>
+/// This component requires <see href="https://nuget.org/packages/reportgenerator">ReportGenerator tool</see>
+/// </remarks>
+public interface IReportUnitTestCoverage : IReportCoverage, IUnitTest
+{
+    internal sealed Configure<ReportGeneratorSettings> ReportGeneratorSettingsBase => _ => _
+        .SetFramework("net5.0")
+        .SetReports(UnitTestResultsDirectory / "*.xml")
+        .SetReportTypes(ReportTypes.Badges, ReportTypes.HtmlChart, ReportTypes.HtmlInline)
+        .WhenNotNull(this.As<IHaveGitRepository>()?.GitRepository,
+            (_, repository) => !string.IsNullOrWhiteSpace(repository.Branch)
+                ? _.SetTargetDirectory(CoverageReportDirectory / repository.Branch)
+                    .SetHistoryDirectory(CoverageReportHistoryDirectory / repository.Branch)
+                    .SetTag(repository.Commit)
+                : _.SetTargetDirectory(CoverageReportDirectory)
+                    .SetHistoryDirectory(CoverageReportHistoryDirectory)
+                    .SetTag(repository.Commit)
+        )
+        .When(_ => this.As<IHaveGitRepository>() is null,
+            _ => _.SetTargetDirectory(CoverageReportDirectory)
+                .SetHistoryDirectory(CoverageReportHistoryDirectory));
+
+    internal sealed Configure<CodecovSettings> CodeCovSettingsBase => _ => _
+        .SetFiles(UnitTestResultsDirectory.GlobFiles("*.xml").Select(x => x.ToString()))
+        .SetToken(CodecovToken)
+        .SetFramework("netcoreapp3.0")
+        .WhenNotNull(this.As<IHaveGitVersion>(),
+            (_, version) => _.SetBuild(version.GitVersion.FullSemVer))
+        .WhenNotNull(this.As<IHaveGitRepository>(),
+            (_, repository) => _.SetBranch(repository.GitRepository.Branch)
+                .SetSha(repository.GitRepository.Commit));
+
+    /// <summary>
+    /// Pushes code coverage to CodeCov
+    /// </summary>
+    public Target ReportUnitTestCoverage => _ => _
+       .DependsOn(UnitTests)
+       .Requires(() => !ReportToCodeCov || CodecovToken != null)
+       .Consumes(UnitTests, UnitTestResultsDirectory / "*.xml")
+       .Produces(CoverageReportDirectory / "*.xml")
+       .Produces(CoverageReportHistoryDirectory / "*.xml")
+       .Executes(() =>
+       {
+           ReportGenerator(s => s.Apply(ReportGeneratorSettingsBase)
+                                 .Apply(ReportGeneratorSettings));
+
+           if (ReportToCodeCov)
+           {
+               Codecov(s => s.Apply(CodeCovSettingsBase)
+                             .Apply(CodecovSettings));
+           }
+       });
+}
