de22ee106421e9eadb287fae32642228eb2f3d83
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-07-27 17:08:37 +0200

    feat(mutation): add HTML reporter to Stryker settings
    - update `StrykerArgumentsSettingsBase` to include StrykerReporter.Html in the reporters list
    - ensure StrykerReporter.Html is added only when IsLocalBuild is true

--- src/Candoumbe.Pipelines/Components/IMutationTest.cs
+++ src/Candoumbe.Pipelines/Components/IMutationTest.cs
@@ -188,7 +188,7 @@ public interface IMutationTest : IHaveTests
 
     internal Configure<StrykerSettings> StrykerArgumentsSettingsBase => options
         => options.When(_ => IsLocalBuild, (settings) => settings.SetOpenReport(StrykerOpenReport.Html)
-                                                                    .SetReporters([..settings.Reporters ?? [], StrykerReporter.Progress]))
+                                                                    .SetReporters([..settings.Reporters ?? [], StrykerReporter.Progress, StrykerReporter.Html]))
             .When(_ => StrykerDashboardApiKey is not null, settings => settings.SetDashboardApiKey(StrykerDashboardApiKey)
                 .SetReporters([..settings.Reporters ?? [], StrykerReporter.Dashboard]));
 
